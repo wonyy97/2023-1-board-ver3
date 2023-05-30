4831a6d1725b3b4063baf06943cda6657e441eab
노정민 <ssolen@naver.com>
2023-05-30 10:41:36 +0900

    user

---
+++ src/main/java/com/green/boardver3/user/UserController.java
@@ -0,0 +1,10 @@
+package com.green.boardver3.user;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class BoardController {
+
+}
---
+++ src/main/java/com/green/boardver3/user/UserService.java
@@ -0,0 +1,4 @@
+package com.green.boardver3.user;
+
+public class BoardService {
+}
