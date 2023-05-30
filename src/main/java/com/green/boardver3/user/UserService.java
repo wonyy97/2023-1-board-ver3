d8497bba72e6ea400994080fac4cebdd1ea39bd2
김원희 <kwh1619@naver.com>
2023-05-30 12:48:29 +0900

    비밀번호 암호화

--- src/main/java/com/green/boardver3/user/UserService.java
+++ src/main/java/com/green/boardver3/user/UserService.java
@@ -2,18 +2,23 @@ package com.green.boardver3.user;
 
 import com.green.boardver3.user.model.UserEntity;
 import com.green.boardver3.user.model.UserInsDto;
+import com.green.boardver3.utils.CommonUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
     private final UserMapper mapper;
+    private final CommonUtils commonUtils;
 
     @Autowired
-    public UserService(UserMapper mapper) {
+    public UserService(UserMapper mapper, CommonUtils commonUtils) {
         this.mapper = mapper;
+        this.commonUtils = commonUtils;
     }
     public int insUser(UserInsDto dto) {
+        String hashPw = commonUtils.encodeSha256(dto.getUpw());
+        dto.setUpw(hashPw);
         return mapper.insUser(dto);
     }
 
