bf25eca0ec1249535e7c2eee3fa2f4aeb319a726
장하은 <haun0@naver.com>
2023-05-30 11:58:24 +0900

    final

--- src/main/java/com/green/boardver3/user/UserService.java
+++ src/main/java/com/green/boardver3/user/UserService.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
-    private UserMapper mapper;
+    private final UserMapper mapper;
 
     @Autowired
     public UserService(UserMapper mapper) {
