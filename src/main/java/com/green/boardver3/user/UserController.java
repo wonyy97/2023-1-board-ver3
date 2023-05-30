858cfc595664ac23e045a1d49acfbc6231a96bcf
노정민 <ssolen@naver.com>
2023-05-30 12:31:06 +0900

    operation

--- src/main/java/com/green/boardver3/user/UserController.java
+++ src/main/java/com/green/boardver3/user/UserController.java
@@ -1,6 +1,7 @@
 package com.green.boardver3.user;
 
 import com.green.boardver3.user.model.UserInsDto;
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -20,9 +21,16 @@ public class UserController {
 
 
     @PostMapping
+    @Operation(summary = "회원가입", description = "" +
+    "uid: [20] 회원 아이디,<br>" +
+    "upw: [100] 회원 비밀번호,<br>" +
+    "nm: [30] 회원명,<br>" +
+    "gender: [1] 성별(M: 남성, F: 여성),<br>" +
+    "addr: [100] 대구시 달서구")
     public int postUser(@RequestBody UserInsDto dto) {
         return service.insUser(dto);
     }
+
 
 
 }
