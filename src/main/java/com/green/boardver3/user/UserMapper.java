6774050931813c7ed2be3e9eacb957821e077afd
장하은 <haun0@naver.com>
2023-05-30 10:57:16 +0900

    mapper

---
+++ src/main/java/com/green/boardver3/user/UserMapper.java
@@ -0,0 +1,7 @@
+package com.green.boardver3.user;
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface UserMapper {
+}
---
+++ src/main/java/com/green/boardver3/user/model/UserInsDto.java
@@ -0,0 +1,12 @@
+package com.green.boardver3.user.model;
+
+import com.green.boardver3.user.UserEntity;
+
+public class UserInsDto extends UserEntity {
+    private String uid;
+    private String upw;
+    private String nm;
+    private char gender;
+    private String addr;
+
+}
