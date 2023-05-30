5f2b6dda92562574938998086f5ca25bae45d621
노정민 <ssolen@naver.com>
2023-05-30 11:10:55 +0900

    controller,service1

--- src/main/java/com/green/boardver3/user/UserService.java
+++ src/main/java/com/green/boardver3/user/UserService.java
@@ -1,7 +1,20 @@
 package com.green.boardver3.user;
 
+import com.green.boardver3.user.model.UserEntity;
+import com.green.boardver3.user.model.UserInsDto;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
+    private UserMapper mapper;
+
+    @Autowired
+    public UserService(UserMapper mapper) {
+        this.mapper = mapper;
+    }
+    public int insUser(UserInsDto dto) {
+        return mapper.insUser(dto);
+    }
+
 }
