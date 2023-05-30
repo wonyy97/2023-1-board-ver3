fc91fb7d88da8b620b755386023f03c10d6c54af
김원희 <kwh1619@naver.com>
2023-05-30 13:09:01 +0900

    성별 대문자, 에러 리턴

--- src/main/java/com/green/boardver3/user/UserService.java
+++ src/main/java/com/green/boardver3/user/UserService.java
@@ -1,10 +1,11 @@
 package com.green.boardver3.user;
 
-import com.green.boardver3.user.model.UserEntity;
 import com.green.boardver3.user.model.UserInsDto;
 import com.green.boardver3.utils.CommonUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+//최대한 자바단에서 다 잡기
 
 @Service
 public class UserService {
@@ -19,12 +20,22 @@ public class UserService {
     public int insUser(UserInsDto dto) {
         //성별 항상 대문자로 받기
         char gender = Character.toUpperCase(dto.getGender());
+        // 성별로 m, f 가 아닌 다른게 들어왔을 때 -1날림
+        if(!(gender == 'M' || gender == 'F')) {
+            return -1;
+        }
         dto.setGender(gender);
 
         //비밀번호 암호화
         String hashPw = commonUtils.encodeSha256(dto.getUpw());
         dto.setUpw(hashPw);
-        return mapper.insUser(dto);
+        //에러 터졌을 때 리턴값으로 0날림
+        try {
+            return mapper.insUser(dto);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return 0;
+        }
     }
 
 }
