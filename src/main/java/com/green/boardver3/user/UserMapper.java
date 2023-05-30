ae857ffa25b454fbc6b1c2b7734adc5992e5e552
장하은 <haun0@naver.com>
2023-05-30 11:08:55 +0900

    mapper

--- src/main/java/com/green/boardver3/user/UserMapper.java
+++ src/main/java/com/green/boardver3/user/UserMapper.java
@@ -1,9 +1,10 @@
 package com.green.boardver3.user;
 
 import com.green.boardver3.user.model.UserEntity;
+import com.green.boardver3.user.model.UserInsDto;
 import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface UserMapper {
-    int insUser(UserEntity entity);
+    int insUser(UserInsDto dto);
 }
