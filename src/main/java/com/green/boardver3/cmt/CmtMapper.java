0177c160de20744a6d764a9b45fd1abffb30fa3d
장하은 <haun0@naver.com>
2023-06-01 12:42:39 +0900

    cmt delete

--- src/main/java/com/green/boardver3/cmt/CmtMapper.java
+++ src/main/java/com/green/boardver3/cmt/CmtMapper.java
@@ -1,9 +1,6 @@
 package com.green.boardver3.cmt;
 
-import com.green.boardver3.cmt.model.CmtEntity;
-import com.green.boardver3.cmt.model.CmtInsDto;
-import com.green.boardver3.cmt.model.CmtSelPageDto;
-import com.green.boardver3.cmt.model.CmtSelPageVo;
+import com.green.boardver3.cmt.model.*;
 import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
@@ -12,4 +9,5 @@ import java.util.List;
 public interface CmtMapper {
     int insCmt(CmtEntity entity);
     List<CmtSelPageVo> selPageCmt(CmtSelPageDto dto);
+    int delCmt(CmtDelDto dto);
 }
---
+++ src/main/java/com/green/boardver3/cmt/model/CmtDelDto.java
@@ -0,0 +1,10 @@
+package com.green.boardver3.cmt.model;
+
+import lombok.Data;
+
+@Data
+public class CmtDelDto {
+    private int iboard;
+    private int iboardCmt;
+    private int iuser;
+}
