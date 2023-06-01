5c436d9f03f98716a3b6ea110046f97f32ba4ea9
노정민 <ssolen@naver.com>
2023-06-01 11:57:33 +0900

    postcmt 수정

--- src/main/java/com/green/boardver3/cmt/model/CmtSelPageVo.java
+++ src/main/java/com/green/boardver3/cmt/model/CmtSelPageVo.java
@@ -1,8 +1,10 @@
 package com.green.boardver3.cmt.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class CmtSelPageVo {
     private int iboardCmt;
     private int iboard;
